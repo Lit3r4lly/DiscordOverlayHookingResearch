9caddaf183c1a838cd8003c2e4a334898655e5cb
Ori <literallynotf@gmail.com>
2020-11-28 19:23:49 +0200

    Updated variables types, names and changed output printing

--- DiscordOverlayHooking/dllmain.cpp
+++ DiscordOverlayHooking/dllmain.cpp
@@ -7,26 +7,32 @@ void HookEx() {
 
     BYTE fnCreateHookPattern[] = "\x40\x53\x55\x56\x57\x41\x54\x41\x56\x41\x57\x48\x83\xEC\x60";
     char fnCreateHookMask[] = "xxxxxxxxxxxxxxx";
+    uintptr_t functionAddr{};
 
     AllocConsole();
     freopen("CONOUT$", "w", stdout);
 
-    std::cout << "[!] Starting to hook discord overlay" << '\n';
+    std::cout << "[!] Starting to hook discord overlay..." << '\n';
 
-    std::cout << "[?] Process Handle - 0x" << (void*)hProcess << '\n';
+    std::cout << "[?] Process handle - 0x" << (void*)hProcess << '\n';
     std::cout << "[?] DiscordHook64.dll handle - 0x" << (void*)hModule << '\n';
-
-    std::cout << "[?] Pattern - " << (BYTE*)fnCreateHookPattern << '\n';
-    std::cout << "[?] Mask - " << fnCreateHookMask << '\n';
+    std::cout << "[?] Pattern - ";
+    for (const auto& val : fnCreateHookPattern) printf("\\x%.2x", val);
+    std::cout << '\n' << "[?] Mask - " << fnCreateHookMask << '\n';
 
     PatternScanning newObj(hProcess, hModule);
-    uintptr_t addr = newObj.PatternScan(fnCreateHookPattern, fnCreateHookMask);
+    functionAddr = newObj.PatternScan(fnCreateHookPattern, fnCreateHookMask);
 
-    std::cout << "[-] CreateHook address - 0x" << (void*)addr << '\n';
-    
+    std::cout << "[-] CreateHook relative address - 0x" << (void*)functionAddr << '\n';
+    std::cout << "[-] CreateHook absolute address - 0x" << (void*)(functionAddr + (uintptr_t)hModule) << '\n';
+
     std::cout << "\n[!] Finished, you can close the console right now." << '\n';
     system("PAUSE");
+
+    fclose(stdout);
     FreeConsole();
+
+    return;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule,
