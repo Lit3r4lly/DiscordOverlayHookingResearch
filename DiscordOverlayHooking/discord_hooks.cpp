14d761df32c8a8c76e5b668603f4c250fd8b4eec
Ori <literallynotf@gmail.com>
2020-11-29 02:57:19 +0200

    Added first implementation of function wrappers for minhook

--- DiscordOverlayHooking/discord_hooks.cpp
+++ DiscordOverlayHooking/discord_hooks.cpp
@@ -1,14 +1,26 @@
 #include "pch.h"
 #include "discord_hooks.h"
 
-void DiscordHook::CreateHook(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal) const {
-
+uintptr_t DiscordHook::CreateHook(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal, uintptr_t createHookAddr) {
+	auto myCreareHook = (wrapCreateHook)createHookAddr;
+	return myCreareHook(pTarget, pDetour, ppOriginal);
 }
 
-void DiscordHook::EnableHook(LPVOID pTarget) const {
-
+uintptr_t DiscordHook::EnableHook(LPVOID pTarget, uintptr_t enableHookAddr) {
+	auto myEnableHook = (wrapEnableHook)enableHookAddr;
+	return myEnableHook(pTarget);
 }
 
-void DiscordHook::EnableHookQueue() const {
+uintptr_t DiscordHook::EnableHookQueue(uintptr_t enableHookQueueAddr) {
+	auto myEnableHook = (wrapEnableHookQueue)enableHookQueueAddr;
+	return myEnableHook();
+}
 
+HRESULT hookedPresentFunction(IDXGISwapChain* dxSwapChain, UINT syncInterval, UINT flags, uintptr_t presentAddr) {
+	DebugBreak();
+	MessageBoxA(nullptr, "Got hooked!", "Hi!", MB_OK);
+	//std::cout << "Hooked" << '\n';
+	auto originalPresent = (wrapPresent)presentAddr;
+
+	return originalPresent(dxSwapChain, syncInterval, flags);
 }
