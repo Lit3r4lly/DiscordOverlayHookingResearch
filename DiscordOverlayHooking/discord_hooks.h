88eb0be479b432048f7dce783292b2e60b352944
Ori <literallynotf@gmail.com>
2020-12-02 00:38:26 +0200

    Moved static class to namespace

--- DiscordOverlayHooking/discord_hooks.h
+++ DiscordOverlayHooking/discord_hooks.h
@@ -4,7 +4,7 @@
 #include <Windows.h>
 #include <dxgi.h>
 
-// Minhook functions callbacks
+// Minhook functions wrappers
 
 using wrapCreateHook = uintptr_t(__fastcall*)(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal);
 using wrapEnableHook = uintptr_t(__fastcall*)(LPVOID pTarget);
@@ -13,14 +13,8 @@ using wrapPresent = HRESULT(__fastcall*)(IDXGISwapChain* dxSwapChain, UINT syncI
 
 HRESULT hookedPresentFunction(IDXGISwapChain* dxSwapChain, UINT syncInterval, UINT flags, uintptr_t presentAddr);
 
-class DiscordHook
-{
-public:
-	DiscordHook() = default;
-	~DiscordHook() = default;
-
-	static uintptr_t CreateHook(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal, uintptr_t createHookAddr);
-	static uintptr_t EnableHook(LPVOID pTarget, uintptr_t enableHookAddr);
-	static uintptr_t EnableHookQueue(uintptr_t enableHookQueueAddr);
-
+namespace DiscordHook {
+	uintptr_t CreateHook(LPVOID pTarget, LPVOID pDetour, LPVOID* ppOriginal, uintptr_t createHookAddr);
+	uintptr_t EnableHook(LPVOID pTarget, uintptr_t enableHookAddr);
+	uintptr_t EnableHookQueue(uintptr_t enableHookQueueAddr);
 };
