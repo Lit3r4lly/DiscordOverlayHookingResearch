2ce3bd5b8c8de7a03d3355b2c5cca37ff81fa1d9
Ori <literallynotf@gmail.com>
2020-12-02 00:38:55 +0200

    Updated types and function calls

--- DiscordOverlayHooking/dllmain.cpp
+++ DiscordOverlayHooking/dllmain.cpp
@@ -3,18 +3,61 @@
 #include "discord_hooks.h"
 
 void HookEx() {
-    const auto hProcess = ::GetCurrentProcess();
-    const auto hModule = ::GetModuleHandle("DiscordHook64.dll");
-    PatternScanning newObj(hProcess, hModule);
-
-    const auto createHookAddr = newObj.PatternScan((std::uint8_t*)"\x40\x53\x55\x56\x57\x41\x54\x41\x56\x41\x57\x48\x83\xEC\x60", (char*)"xxxxxxxxxxxxxxx");
-    const auto enableHookAddr = newObj.PatternScan((std::uint8_t*)"\x48\x89\x5C\x24\x00\x48\x89\x6C\x24\x00\x48\x89\x74\x24\x00\x57\x41\x56\x41\x57\x48\x83\xEC\x20\x33\xF6\x8B\xFA", (char*)"xxxx?xxxx?xxxx?xxxxxxxxxxxxx");
-    const auto enableHookQueueAddr = newObj.PatternScan((std::uint8_t*)"\x48\x89\x5C\x24\x00\x48\x89\x6C\x24\x00\x48\x89\x7C\x24\x00\x41\x57", (char*)"xxxx?xxxx?xxxx?xx");
-    const auto presentAddr = newObj.PatternScan((std::uint8_t*)"\x48\x89\x5C\x24\x00\x48\x89\x74\x24\x00\x57\x48\x83\xEC\x20\x48\x8B\xD9\x41\x8B\xF8", (char*)"xxxx?xxxx?xxxxxxxxxxx");
-    const auto originalPresent = (wrapPresent)nullptr;
-
     AllocConsole();
     freopen("CONOUT$", "w", stdout);
+
+    uintptr_t createHookAddr{};
+    uintptr_t enableHookAddr{};
+    uintptr_t enableHookQueueAddr{};
+    uintptr_t presentAddr{};
+    const auto originalPresent = nullptr;
+
+    const std::uint8_t* createHookPattern = (std::uint8_t*)"\x40\x53\x55\x56\x57\x41\x54\x41\x56\x41\x57\x48\x83\xEC\x60";
+    const std::uint8_t* enableHookPattern = (std::uint8_t*)"\x48\x89\x5C\x24\x00\x48\x89\x6C\x24\x00\x48\x89\x74\x24\x00\x57\x41\x56\x41\x57\x48\x83\xEC\x20\x33\xF6\x8B\xFA";
+    const std::uint8_t* enableHookQueuePattern = (std::uint8_t*)"\x48\x89\x5C\x24\x00\x48\x89\x6C\x24\x00\x48\x89\x7C\x24\x00\x41\x57";
+    const std::uint8_t* presentPattern = (std::uint8_t*)"\x48\x89\x5C\x24\x00\x48\x89\x74\x24\x00\x57\x48\x83\xEC\x20\x48\x8B\xD9\x41\x8B\xF8";
+
+    const std::string createHookMask = "xxxxxxxxxxxxxxx";
+    const std::string enableHookMask = "xxxx?xxxx?xxxx?xxxxxxxxxxxxx";
+    const std::string enableHookQueueMask = "xxxx?xxxx?xxxx?xx";
+    const std::string presentMask = "xxxx?xxxx?xxxxxxxxxxx";
+
+    const auto hProcess = ::GetCurrentProcess();
+    const auto hModule = ::GetModuleHandle("DiscordHook64.dll");
+
+    if (hModule == INVALID_HANDLE_VALUE) {
+        std::cerr << "Invalid module handle!" << '\n';
+        system("pause");
+
+        fclose(stdout);
+        FreeConsole();
+        return;
+    }
+
+    if (hProcess == INVALID_HANDLE_VALUE) {
+        std::cerr << "Invalid process handle!" << '\n';
+        system("pause");
+
+        fclose(stdout);
+        FreeConsole();
+        return;
+    }
+
+    PatternScanning newObj(hProcess, hModule);
+
+    try {
+        createHookAddr = newObj.PatternScan(createHookPattern, createHookMask);
+        enableHookAddr = newObj.PatternScan(enableHookPattern, enableHookMask);
+        enableHookQueueAddr = newObj.PatternScan(enableHookQueuePattern, enableHookQueueMask);
+        presentAddr = newObj.PatternScan(presentPattern, presentMask);
+    } catch (const std::string& message) {
+        std::cerr << message << '\n';
+        system("pause");
+
+        fclose(stdout);
+        FreeConsole();
+        return;
+    }
 
     std::cout << "[!] Starting to hook discord overlay..." << '\n';
 
@@ -26,10 +69,9 @@ void HookEx() {
     std::cout << "[-] Enable Hook Queue absolue address - 0x" << (void*)(enableHookQueueAddr) << '\n';
     std::cout << "[-] Present absolue address - 0x" << (void*)(presentAddr) << '\n';
 
-    DiscordHook discord;
-    discord.CreateHook((LPVOID)presentAddr, (LPVOID)hookedPresentFunction, (LPVOID*)&originalPresent, createHookAddr);
-    discord.EnableHook((LPVOID)presentAddr, enableHookAddr);
-    discord.EnableHookQueue(enableHookQueueAddr);
+    DiscordHook::CreateHook((LPVOID)presentAddr, (LPVOID)hookedPresentFunction, (LPVOID*)&originalPresent, createHookAddr);
+    DiscordHook::EnableHook((LPVOID)presentAddr, enableHookAddr);
+    DiscordHook::EnableHookQueue(enableHookQueueAddr);
 
     std::cout << "\n[!] Finished, you can close the console right now." << '\n';
     system("PAUSE");
